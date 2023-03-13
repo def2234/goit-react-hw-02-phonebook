75f327271acdefb2b5f84aafec40edc0b6407803
def2234 <def22349@gmail.com>
2023-03-13 22:27:32 +0100

    Complited Homework 2 Phonebook

---
+++ src/components/ContactsList/ContactsList.js
@@ -0,0 +1,32 @@
+import PropTypes from 'prop-types';
+import { Btn } from './ContactsList-styled';
+
+export const ContactsList = ({ contacts, onDeleteContact }) => {
+  return (
+    <ul>
+      {contacts.map(contact => {
+        return (
+          <li key={contact.id}>
+            {contact.name}: {contact.number}
+            <Btn type="button" onClick={() => onDeleteContact(contact.id)}>
+              Delete
+            </Btn>
+          </li>
+        );
+      })}
+    </ul>
+  );
+};
+
+ContactsList.propTypes = {
+  contacts: PropTypes.arrayOf(
+    PropTypes.shape({
+      name: PropTypes.string.isRequired,
+      number: PropTypes.string.isRequired,
+    })
+  ),
+  onDeleteContact: PropTypes.func.isRequired,
+  onClick: PropTypes.shape({
+    id: PropTypes.string.isRequired,
+  }),
+};
